35d0785257a8401995542c97187f9e2ecf11f7d5
Neeraj Gupta <theneerajgupta@gmail.com>
2025-11-11 12:34:24 +0530

    purge: starting fresh

--- src/core/index.ts
+++
@@ -1,6 +0,0 @@
-// core exports
-// these are the foundational utils that will power this package
-
-export { transform, hasResponsiveProps } from './transform';
-export { BREAKPOINT_MAP } from './types';
-export type { ResponsiveProps, Breakpoint } from './types';
--- test-app/src/TestComponent.tsx
+++
@@ -1,7 +0,0 @@
-export function TestComponent() {
-  return (
-    <div className="mt-8 p-4 bg-blue-500" md="bg-yellow-500">
-      <p>Manual test - this should change colors on resize</p>
-    </div>
-  )
-}
